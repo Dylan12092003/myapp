5d101e9c73a5046f0d91d2884c9bd92cba492ee5
Dylan Nuñez <dylan.nunezp@pca.edu.co>
2024-02-03 17:12:17 -0500

    Cambios

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -9,14 +9,17 @@ export class AuthService {
 
   loginUser(credential: any) {
     return new Promise((accept, reject) => {
-      if (credential.email === 'nunezdylan624@gmail.com' && credential.password === '0813121722') {
+      if (
+        (credential.email === 'nunezdylan624@gmail.com' && credential.password === '0813121722') ||
+        (credential.email === 'dylannunezpenaloza400@gmail.com' && credential.password === '12345')
+      ) {
         accept('Login correcto');
-      } else if (credential.email === 'nunezdylan624@gmail.com') {
+      } else if (credential.email === 'nunezdylan624@gmail.com' || credential.email === 'dylannunezpenaloza@gmail.com') {
         reject('Contraseña incorrecta');
       } else {
         reject('Login incorrecto');
       }
     });
   }
-  
 }
+
